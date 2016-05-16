9b19f8fc2736ee122b5978eea07d9d883d852413
AliceTed <week270@yahoo.co.jp>
2016-05-16 09:56:56 +0900

    更新

--- hosihime/hosihime/Source/Title.cpp
+++ hosihime/hosihime/Source/Title.cpp
@@ -18,37 +18,42 @@ void Title::Init()
 void Title::Update()
 {
 	//メニュー選択の処理
-	if (boolSpace){
-		if (gsGetKeyState(GKEY_UPARROW)){
-			if (gsGetKeyTrigger(GKEY_UPARROW)){
+	if (boolSpace)
+	{
+		if (gsGetKeyState(GKEY_UPARROW))
+		{
+			if (gsGetKeyTrigger(GKEY_UPARROW))
+			{
 				index -= 1;
 			}
 		}
-		if (gsGetKeyState(GKEY_DOWNARROW)){
-			if (gsGetKeyTrigger(GKEY_DOWNARROW)){
+		if (gsGetKeyState(GKEY_DOWNARROW))
+		{
+			if (gsGetKeyTrigger(GKEY_DOWNARROW))
+			{
 				index += 1;
 			}
 		}
 		if (index > 2)index = 0;
 		if (index < 0)index = 2;
-		isEnd = gsGetKeyTrigger(GKEY_SPACE);
-}
+		isEnd = !!gsGetKeyTrigger(GKEY_SPACE);
+	}
 
 	//スペースキーを押すとメニューを表示
-	if (gsGetKeyState(GKEY_SPACE)&& !boolSpace){
-		
+	if (gsGetKeyState(GKEY_SPACE) && !boolSpace){
+
 		boolSpace = true;
 	}
 }
 void Title::Draw(Renderer& renderer)
 {
 	renderer.DrawTextrue("title.bmp", &GSvector2(0, 0));
-	renderer.DrawTextrue("titlename.bmp", &GSvector2(200,120));
+	renderer.DrawTextrue("titlename.bmp", &GSvector2(200, 120));
 	if (!boolSpace){
 		renderer.DrawTextrue("spacename.bmp", &GSvector2(480, 540));
-}
+	}
 	else{
-		
+
 		renderer.DrawTextrue("gs_g.bmp", &GSvector2(450, 350));
 		renderer.DrawTextrue("turt_g.bmp", &GSvector2(450, 450));
 		renderer.DrawTextrue("credit_g.bmp", &GSvector2(450, 550));
@@ -75,23 +80,23 @@ void Title::Finish()
 }
 Scene Title::Next()
 {
-	
+	Scene ns;
 	switch (index)
 	{
 	case 0:
 		//ゲームプレイへ
-	return MODE_GAMEPLAY;
+		ns= MODE_GAMEPLAY;
 		break;
 	case 1:
 		//そうさせつめいへ(今はエンディングへ飛ぶ)
-		return MODE_ENDING;
+		ns = MODE_ENDING;
 		break;
 	case 2:
 		//せいさくしゃ一覧へ（今はエンディングへ飛ぶ）
-		return MODE_ENDING;
+		ns = MODE_ENDING;
 		break;
 	}
-	
+	return ns;
 }
 bool Title::IsEnd()
 {
